3248fb6b8cee58c7a73d076a6c17a3d89096ebaf
az236 <az23634@gmail.com>
2023-05-24 21:56:26 +0300

    Major update

---
+++ database/migrations/2023_05_15_061735_add_bus_company_to_destinations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('destinations', function (Blueprint $table) {
+            $table->unsignedBigInteger("busCompany")->nullable();
+            $table->foreign('busCompany')->references('id')->on('bus_companies')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('destinations', function (Blueprint $table) {
+            $table->dropColumn("busCompany");
+        });
+    }
+};
