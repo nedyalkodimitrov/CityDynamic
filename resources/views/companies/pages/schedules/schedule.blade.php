f57b9acd8e8488ff5ecc1eef3b1a8a4444309d07
NEDYALKO DIMITROV <az23634@gmail.com>
2024-03-04 21:11:26 +0200

    Added resources and requests

---
+++ resources/views/companies/pages/schedules/schedule.blade.php
@@ -0,0 +1,12 @@
+@extends('companies.layouts.master')
+@section('content')
+    <h1 class="col-12 text-center mb-2">Предписание</h1>
+
+    <div class="card col-12 mb-2 ">
+        <div class="col-12 col-md-9 mx-auto col-lg-12  p-4" method="post">
+
+        </div>
+    </div>
+
+@endsection
+
